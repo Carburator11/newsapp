8476cb3313b0f815de777f94e5dad2a080a1d394
Pierre Vaut <P.vautherin@hotmail.com>
2017-10-27 13:53:43 +0200

    stuff getting serious

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var https = require("https");
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -8,11 +9,38 @@ app.get('/', function(req, res) {
   res.end("blablabla");
 });
 
+
+
+var cnnUrl = 'https://newsapi.org/v1/articles?source=cnn&sortBy?&apiKey=c6b3fe2e86d54cae8dcb10dc77d5c5fc';
+var cnnJson = '';
+
+https.get(cnnUrl, (res) => {
+    var body = '';
+    res.on('data', function(chunk){
+      body += chunk;
+    });
+
+    res.on('end', function(){
+        cnnJson = JSON.parse(body);
+        console.log('Json file scrapped: ', JSON.stringify(cnnJson).substring(1, 99), '....');
+    });
+
+}).on('error', function(e){
+  console.log('Got an error', e);
+  });
+
+
+
+
+
 app.get('/freaks/:freaksnum', function(req, res) {
   res.setHeader('Content-Type', 'text/html');
   var glutt = ["Molly Joe", "Ticky", "Lewis Jr"];
-  res.render('freaks.ejs',{freaksnum: req.params.freaksnum, glutt: glutt});
+  var baum  = JSON.stringify(cnnJson).substring(1, 999) + ".....     and a lot more to come";
+  res.render('freaks.ejs',{freaksnum: req.params.freaksnum, glutt: glutt, baum: baum});
+
 });
+
 
 
 
