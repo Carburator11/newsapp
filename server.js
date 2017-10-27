07a8c2acc102eb1c1f84825e28ebc13a9fbe78f1
Pierre Vaut <P.vautherin@hotmail.com>
2017-10-27 10:48:04 +0200

    adding gitignore file

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 var express = require('express');
-
 var app = express();
 
 app.get('/', function(req, res) {
-  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Type', 'text/html');
   res.end("blablabla");
 });
 
 app.get('/freaks/:freaksnum', function(req, res) {
   res.setHeader('Content-Type', 'text/html');
-  res.render('freaks.ejs',{freaksnum: req.params.freaksnum});
+  var glutt = ["Molly Joe", "Ticky", "Lewis Jr"];
+  res.render('freaks.ejs',{freaksnum: req.params.freaksnum, glutt: glutt});
 });
+
+
 
 app.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
