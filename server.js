b731f45c294250112659ac490190eb99d8c32f07
Pierre Vaut <P.vautherin@hotmail.com>
2017-10-27 13:03:44 +0200

    more stuff...

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res) {
   res.setHeader('Content-Type', 'text/html');
   res.end("blablabla");
@@ -19,6 +21,6 @@ app.use(function(req, res, next){
     res.status(404).send('Page introuvable !');
 });
 
-app.listen(8000, () => {
-  console.log('We are live on port 8000...');
+app.listen(app.get('port'), () => {
+  console.log('We are live on port: ', app.get('port'));
 });
