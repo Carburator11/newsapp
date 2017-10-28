d75b48b1b6b136d92164cdce22edf5750b14a6af
Pierre Vaut <P.vautherin@hotmail.com>
2017-10-28 12:44:32 +0200

    fix timer module

---
+++ my_modules/timer/timer.js
@@ -0,0 +1,23 @@
+
+
+
+module.exports ={
+  timer: function(e){
+      let now = new Date();
+      let sec = now.getSeconds();
+      let min = now.getMinutes();
+      let hh  = now.getHours();
+      let dd  = now.getDate();
+      let mm  = now.getMonth()+1; //January is 0!
+      let yyyy = now.getFullYear();
+      if(sec<10){ sec='0'+sec;}
+      if(min<10){ min='0'+min;}
+      if(hh<10){ hh='0'+hh;}
+      if(dd<10){ dd='0'+dd;}
+      if(mm<10){ mm='0'+mm;}
+      var dateFull = hh+":"+min+":"+sec+" - "+dd+'/'+mm+'/'+yyyy;
+      var dateDay = yyyy+mm+dd;
+      if(e = ""){return dateDay;}
+      else {return dateFull}
+      }
+}
