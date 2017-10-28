c8ad84ba10fd939f079da406354767afcbf1dde5
Pierre Vaut <P.vautherin@hotmail.com>
2017-10-28 12:15:01 +0200

    admin tool

--- server.js
+++ server.js
@@ -1,48 +1,34 @@
 var express = require('express');
 var app = express();
 var https = require("https");
+var timer = require("./node_modules/timer/timer");
+var fs = require("fs");
+var http = require('http');
+var server = http.createServer(app);
+var bodyParser = require('body-parser');
+
 
 app.set('port', (process.env.PORT || 5000));
+      var cnnJson = 'Json comes here';
+      var cnnUrl = 'https://newsapi.org/v1/articles?source=cnn&sortBy?&apiKey=c6b3fe2e86d54cae8dcb10dc77d5c5fc';
+      var lastUpdate = "00:00:00 - dd/mm/yyyy";
+      var history = [];   //previous requests
+      var glutt = '';
 
-app.get('/', function(req, res) {
+app.use(bodyParser.urlencoded({  extended: true }));
+app.use(bodyParser.json());
+
+app.post('/admin/refresh', function(req, res) {
   res.setHeader('Content-Type', 'text/html');
-  res.end("blablabla");
+  console.log(req.body, 'It worked!');
+  getcnn();
+  res.redirect(303, '/admin');
 });
 
-
-
-var cnnUrl = 'https://newsapi.org/v1/articles?source=cnn&sortBy?&apiKey=c6b3fe2e86d54cae8dcb10dc77d5c5fc';
-var cnnJson = '';
-
-https.get(cnnUrl, (res) => {
-    var body = '';
-    res.on('data', function(chunk){
-      body += chunk;
-    });
-
-    res.on('end', function(){
-        cnnJson = JSON.parse(body);
-        console.log('Json file scrapped: ', JSON.stringify(cnnJson).substring(1, 99), '....');
-    });
-
-}).on('error', function(e){
-  console.log('Got an error', e);
-  });
-
-
-
-
-
-app.get('/freaks/:freaksnum', function(req, res) {
+app.get('/admin', function(req, res) {
   res.setHeader('Content-Type', 'text/html');
-  var glutt = ["Molly Joe", "Ticky", "Lewis Jr"];
-  var baum  = JSON.stringify(cnnJson).substring(1, 999) + ".....     and a lot more to come";
-  res.render('freaks.ejs',{freaksnum: req.params.freaksnum, glutt: glutt, baum: baum});
-
+  res.render('freaks.ejs',{glutt: glutt, lastUpdate: lastUpdate});
 });
-
-
-
 
 app.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
@@ -51,4 +37,28 @@ app.use(function(req, res, next){
 
 app.listen(app.get('port'), () => {
   console.log('We are live on port: ', app.get('port'));
+  getcnn();
 });
+
+
+
+var getcnn = function(){
+      https.get(cnnUrl, (res) => {
+        var body = '';
+        res.on('data', function(chunk){
+          body += chunk;
+        });
+
+        res.on('end', function(){
+            cnnJson = JSON.parse(body);
+            console.log(JSON.stringify(cnnJson).substring(1, 14).replace(/"/g, " "));
+            lastUpdate = timer.timer();
+            console.log('last update :', lastUpdate);
+            glutt  =  (JSON.stringify(cnnJson)).replace(/,|}|]/g, "<br>");
+
+              });
+
+        }).on('error', function(e){
+            console.log('Got an error', e);
+  });
+}
